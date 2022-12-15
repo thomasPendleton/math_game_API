558fa9e0888ba137145ccf6bd261cffaceee42ff
Thomas Pendleton <thomas6576@yahoo.com>
2022-12-15 14:38:38 -0800

    add knex and routes

--- server.js
+++ server.js
@@ -1,20 +1,57 @@
-import express from "express"
+const cors = require("cors")
+const express = require("express")
 const app = express()
-import cors from "cors"
+
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "home",
+    database: "postgres",
+  },
+})
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
 app.get("/", (req, res) => {
-  res.send("server is up and running")
+  knex("highscores")
+    .orderBy('score', 'desc')
+    .where('score', '>', 10)
+    .returning("*")
+    .then((entries) => {
+      res.json(entries)
+    })
 })
 
-app.post('/score', (req, res) => {
-    const {gameTime, correct} = req.body
-    console.log(gameTime, correct)
+app.post("/score", (req, res) => {
+  const { playerName, gameTime, correct, wrong, operation, level } = req.body
+
+  knex("highscores")
+    .insert({
+      name: playerName,
+      gametime: gameTime,
+      score: correct,
+      operator: operation,
+      level: level,
+      missed: wrong,
+      date: new Date(),
+    })
+    .returning("*")
+    .then((users) => {
+      res.status(200).json(users)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json('unable to add score')
+    })
+
+
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on ${process.env.PORT || 3000}`)
+app.listen(process.env.PORT || 3005, () => {
+  console.log(`app is running on ${process.env.PORT || 3005}`)
 })
